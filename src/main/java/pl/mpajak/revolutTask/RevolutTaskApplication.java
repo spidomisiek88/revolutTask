ed5d0f02855d3e94d5360719253f9edb44e938b5
spidomisiek88 <michal.pajak88@gmail.com>
2019-07-06 18:02:29 +0200

    first commit

---
+++ src/main/java/pl/mpajak/revolutTask/RevolutTaskApplication.java
@@ -0,0 +1,13 @@
+package pl.mpajak.revolutTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolutTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevolutTaskApplication.class, args);
+	}
+
+}
